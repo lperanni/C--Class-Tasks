8162b00aeb2d3bcab5362270af88ff9bff38de95
lperanni <eminem.95@hotmail.de>
2020-03-06 15:39:11 +0100

    Second task done

---
+++ Vjezba2/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labs
+{
+    class Triangle : Shape
+    {
+        private double oneSide;
+
+        public Triangle(double p)
+        {
+            oneSide = p;
+            xPos = DataModel.getNewXPos();
+            yPos = DataModel.getNewYPos();
+        }
+        public override double getArea()
+        {
+            return Math.Pow(oneSide, 2) * (Math.Sqrt(3) / 4);
+        }
+
+        public override double getPerimeter()
+        {
+            return oneSide * 3;
+        }
+
+        public override void printData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("My type: " + this.GetType());
+            Console.WriteLine("Side of triangle = " + oneSide);
+            Console.WriteLine("Area of triangle =  " + getArea());
+            Console.WriteLine("Perimeter of triangle =  " + getPerimeter());
+            Console.WriteLine("X position = " + xPos);
+            Console.WriteLine("Y position = " + yPos);
+        }
+    }
+}
